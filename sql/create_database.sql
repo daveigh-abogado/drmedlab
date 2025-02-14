ff235adcc3d1415bacff11ef701fe14b4fee2d63
daveigh-abogado <daveigh.abogado@student.ateneo.edu>
2025-02-14 14:09:31 +0800

    modified description in create_database.sql file

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -1,4 +1,4 @@
--- This is a one time use file. Do not run this twice unless you want to make migrations
+-- This is ideally a one time use file. Do not run this twice unless you want to make migrations
 -- all over again.
 
 Drop database drmedlabs;
