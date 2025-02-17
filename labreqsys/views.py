e454dbd666e23312f98915a0221c7cbeed3ef7d9
d00mplings <164989222+d00mplings@users.noreply.github.com>
2025-02-17 14:06:20 +0800

    patientList extend base

--- labreqsys/views.py
+++ labreqsys/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def base(request):
     return render(request, 'labreqsys/base.html')
+
+def patientList(request):
+    return render(request, 'labreqsys/patientList.html')
