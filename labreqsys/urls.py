b5706694c62e1cc35df28d9fc2afd11f50a07730
daveigh-abogado <daveigh.abogado@student.ateneo.edu>
2025-02-25 07:38:09 +0800

    fixed form flow

--- labreqsys/urls.py
+++ labreqsys/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('patientList', views.patientList, name='patientList'),
     path('view_patient/<int:pk>/', views.view_patient, name='view_patient'),
     path('add_labreq/<int:pk>/', views.add_labreq, name = 'add_labreq'),
+    path('add_labreq_details/<int:pk>/', views.add_labreq_details, name = 'add_labreq_details'),
     path('summarize_labreq/<int:pk>/', views.summarize_labreq, name = 'summarize_labreq')
 ]
