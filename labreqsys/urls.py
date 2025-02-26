7dca7fbb5e716e1d0717bed5455b6297fe5fc1a7
Nellbert Ethan Pineda <nellbert.pineda@student.ateneo.edu>
2025-02-26 12:22:48 +0800

    Forward base url to patient list

--- labreqsys/urls.py
+++ labreqsys/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.base, name='base'),
+    path('', RedirectView.as_view(url='/patientList', permanent=True)),  # Redirect root URL to /patientList
     path('patientList', views.patientList, name='patientList'),
     path('view_patient/<int:pk>/', views.view_patient, name='view_patient'),
     path('add_labreq/<int:pk>/', views.add_labreq, name = 'add_labreq'),
