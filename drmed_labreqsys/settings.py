db5a89524e62e08a6ef1bfd5118cb53b74f1a1cd
Madison Alyssa Velasco <168917783+madicakkes@users.noreply.github.com>
2025-05-13 01:50:41 +0800

    idk why do i need to this pero sige?

--- drmed_labreqsys/settings.py
+++ drmed_labreqsys/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'drmedlabs',
         'USER': 'root',
-        'PASSWORD': '', #Please enter your root password here (you should have this when you set up your SQL client
+        'PASSWORD': 'Kamja_Ace1999', #Please enter your root password here (you should have this when you set up your SQL client
         'HOST':'localhost',
         'PORT':'3306',
     }
